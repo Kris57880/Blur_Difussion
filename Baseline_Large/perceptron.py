d523bd1a7e3ed15f65031dc768814b8c77be94b1
Kris57880 <axzzreety1800@gmail.com>
2023-05-25 10:48:59 +0800

    fix baseline + adding P2 version
    1. modify normalizing mean and variance
    2. adding perception (P2) weighting version

--- Baseline_Large/perceptron.py
+++ Baseline_Large/perceptron.py
@@ -1,41 +1,40 @@
-
 import numpy as np
 import torch
-import torchsummary
-from torchsummary import summary
 import sys
 from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import torchvision
 import os
-from diffusers import DDIMScheduler, DDPMScheduler, UNet2DModel
+from diffusers import DDPMScheduler, UNet2DModel
 from matplotlib import pyplot as plt
 from PIL import Image
 from torchvision import transforms
 from tqdm.auto import tqdm
 import csv
-os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "cpu"
+)
 print('Using device:', device)
 
 image_size = 256
-batch_size = 6
+batch_size = 2
 num_workers = 4
 n_epochs = 200
 lr = 1e-5
-Dataset_dir = 'Dataset/GOPRO_256'
-root_dir = 'Baseline_Large'
-model_dir = root_dir+'/saved_model'
-result_dir = root_dir+'/result'
-log_file = root_dir+'/saved_model/training_log.csv'
+Dataset_dir = 'Dataset'
+model_dir = 'DDDAAA_per/saved_model'
+result_dir = 'DDDAAA_per/result'
+log_file = 'DDDAAA_per/training_log.csv'
 
 preprocess = transforms.Compose([
+    transforms.Resize(image_size),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5]),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
 ])
-
 
 class GOPRO(Dataset):
     def __init__(self, mode='train', transform=preprocess):
@@ -47,15 +46,14 @@ class GOPRO(Dataset):
         self.dirs_gt = []
         self.index = 0
         self.d = 0
-        self.seed_is_set = False
-        self.ordered = False
-        for d1 in os.listdir(os.path.join(Dataset_dir, mode)):
-            for d3 in os.listdir(os.path.join(Dataset_dir, mode, d1, 'blur_gamma')):
+
+        for d1 in os.listdir(os.path.join('./Dataset', mode)):
+            for d3 in os.listdir(os.path.join('./Dataset', mode, d1, 'blur_gamma')):
                 if d3.endswith('.png'):
                     self.dirs.append(os.path.join(
-                        Dataset_dir, mode, d1, 'blur_gamma', d3))
+                        './Dataset', mode, d1, 'blur_gamma', d3))
                     self.dirs_gt.append(os.path.join(
-                        Dataset_dir, mode, d1, 'sharp', d3))
+                        './Dataset', mode, d1, 'sharp', d3))
 
     def __len__(self):
         return len(self.dirs)
@@ -64,14 +62,16 @@ class GOPRO(Dataset):
         self.cur_dirs = self.dirs[index]
         self.cur_dirs_gt = self.dirs_gt[index]
 
+        # image_seq = []
+        # image_seq_gt = []
+
         im = self.transform(Image.open(self.cur_dirs)).reshape(
             (3, image_size, image_size))
-        
+        # image_seq.append(im)
+        # image_seq = torch.Tensor(np.concatenate(image_seq, axis=0))
         im_gt = self.transform(Image.open(self.cur_dirs_gt)).reshape(
             (3, image_size, image_size))
         return im, im_gt
-    
-    
 
 class ClassConditionedUnet(nn.Module):
     def __init__(self):
@@ -86,7 +86,7 @@ class ClassConditionedUnet(nn.Module):
             in_channels=3 + 3,  # Additional input channels for class cond.
             out_channels=3,           # the number of output channels
             layers_per_block=2,       # how many ResNet layers to use per UNet block
-            block_out_channels=(128,128,256,256,512,512),
+            block_out_channels=(128, 128, 256, 256, 512, 512),
             down_block_types=(
                 "DownBlock2D",
                 "DownBlock2D",
@@ -121,20 +121,21 @@ class ClassConditionedUnet(nn.Module):
         return self.model(net_input, t).sample  # (bs, 3 + 1, 256, 256)
 if __name__ == '__main__':
     train_dataset = GOPRO(mode='train')
-    test_dataset = GOPRO(mode='test')
+    # test_dataset = GOPRO(mode='test')
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size,
             num_workers=num_workers, shuffle=True, drop_last=True, pin_memory=True)
 
-    print(f'train data num: {len(train_dataset)} test data num: {len(test_dataset)}')
+    # print(f'train data num: {len(train_dataset)} test data num: {len(test_dataset)}')
     print(f'image size: {image_size} batch size: {batch_size} num workers: {num_workers}')
 
     noise_scheduler = DDPMScheduler(
         num_train_timesteps=1000, beta_schedule='linear')
-
+    alphas = noise_scheduler.alphas_cumprod
+    snr = (alphas / (1-alphas))
     model_files = [f for f in os.listdir(model_dir) if os.path.isfile(os.path.join(model_dir, f)) and f.startswith('epoch_')]
-    #net = ClassConditionedUnet().to(device)
+    net = ClassConditionedUnet().to(device)
     # uncomment if want using multiple GPUs
-    net = nn.DataParallel(ClassConditionedUnet(),device_ids=[0,1]).to(device)
+    #net = nn.DataParallel(ClassConditionedUnet()).to(device)
 
     opt = torch.optim.Adam(net.parameters(), lr=lr)
     start_epoch = 0 
@@ -165,18 +166,20 @@ if __name__ == '__main__':
 
     # Keeping a record of the losses for later viewing
     epoch_losses = []
-    min_loss = 1 
+
     net.train()
 
-    
+    min_loss = float('inf')
     # The training loop
-    for epoch in range(start_epoch+1, n_epochs):
+    for epoch in range(start_epoch+1, n_epochs+1):
         iter_losses = []
+        losses_w = []
         for blur_img, gt in tqdm(train_dataloader):
             # Get some data and prepare the corrupted version
-            blur_img = blur_img.to(device)   # Normalize the data to [-1, 1]
-            gt = gt.to(device)  # Normalize the data to [-1, 1]
+            blur_img = blur_img.to(device) # Normalize the data to [-1, 1]
+            gt = gt.to(device)# Normalize the data to [-1, 1]
             noise = torch.randn_like(gt)
+            # print(torch.min(blur_img))
             timesteps = torch.randint(0, 999, (gt.shape[0],)).long().to(device)
             noisy_x = noise_scheduler.add_noise(gt, noise, timesteps)
 
@@ -186,26 +189,31 @@ if __name__ == '__main__':
 
             # Calculate the loss
             loss = loss_fn(pred, noise)  # How close is the output to the noise
+            snr1 = snr[timesteps].to(device)
+            # print(snr)
+            # temp = ((1 + snr1)**0.5)
+            # snr1 = abs(snr(pred, noise).to(device))
+            # print('snr:',snr)
+            loss_w = (loss / ((1 + snr1)**1)).sum()
             # Backprop and update the params:
             opt.zero_grad()
-            loss.backward()
+            loss_w.backward()
             opt.step()
 
             # Store the loss for later
+            losses_w.append(loss_w.item())
             iter_losses.append(loss.item())
 
         # Print our the average of the last 100 loss values to get an idea of progress:
-        avg_loss = sum(iter_losses[-100:])/100
-        print(f'Finished epoch {epoch}. Loss values: {avg_loss:05f}')
+        avg_loss = sum(iter_losses)/len(iter_losses)
+        epoch_losses.append(avg_loss)
+        print(f'Finished epoch {epoch}. Loss values: {avg_loss:05f} / {sum(losses_w)/len(losses_w):05f}')
         with open(log_file, 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow([epoch, avg_loss])
-        if epoch%10 == 0 :
+        if epoch %10 == 0:
             torch.save(net.state_dict(), model_dir+f'/epoch_{epoch}.pt')
-        if avg_loss<min_loss:
-            min_loss = avg_loss
-            torch.save(net.state_dict(), model_dir+f'/best.pt')
-        epoch_losses.append(avg_loss)
+
     torch.save(net.state_dict(), model_dir+f'/final.pt')
     # View the loss curve
     plt.plot(epoch_losses)
